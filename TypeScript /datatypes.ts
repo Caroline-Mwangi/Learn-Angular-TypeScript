7e048966e058a8f4cabf7ae84aeeb2d65f4d56f5
Caroline-Mwangi <cwmwangi50@gmail.com>
2024-01-30 16:56:41 +0300

    typescript functions

--- TypeScript /datatypes.ts
+++ TypeScript /datatypes.ts
@@ -2,7 +2,7 @@
 
 let lname: string;
 
-lname = 'Doe';
+lname = "Doe";
 
 let newname = lname.toUpperCase();
 
@@ -14,7 +14,7 @@ let age: number;
 
 age = 20;
 
-let result = parseInt("87") // Converts string to number
+let result = parseInt("87"); // Converts string to number
 
 console.log(result);
 
@@ -24,11 +24,11 @@ let isValid: boolean = false; // The boolean valid must be defined
 
 // Arrays
 
-let myList : string[]; // An array that only contains strings
+let myList: string[]; // An array that only contains strings
 
-let myArray : Array<number>; // Another way to define an array
+let myArray: Array<number>; // Another way to define an array
 
-myArray = [1,2,3,4,5];
+myArray = [1, 2, 3, 4, 5];
 
 let result2 = myArray.filter((x) => x > 2); // Filtering numbers that are greater than 2
 
@@ -36,13 +36,12 @@ let sum = myArray.reduce((x, y) => x + y); // Finding the sum of the array of nu
 
 console.log(sum);
 
-
 //Enums: constants based data structure that store a set of named constants grouped around a central theme.
 
 const enum Color {
-    Red,
-    Green,
-    Blue
+  Red,
+  Green,
+  Blue,
 }
 
 let c: Color = Color.Blue;
@@ -51,7 +50,7 @@ let c: Color = Color.Blue;
 
 let myTuple: [number, string, boolean];
 
-myTuple = [3, 'Hello', true];
+myTuple = [3, "Hello", true];
 
 // Any: Any value can be assigned to the variable with this data type. It is not advised to use it in production.
 
@@ -59,9 +58,3 @@ let note: any;
 
 note = "5";
 note = 5;
-
-
-
-
-
-
---
+++ TypeScript /functions.ts
@@ -0,0 +1,28 @@
+function add (a: number, b: number, c?:number) : number{
+    return a + b;
+}
+
+// The parameter c is an optional parameter - it could be null or could have a value
+let mySum = add(3,4);
+console.log(mySum);
+
+// Another way to write typescript functions
+
+let sub = (x:number, y:number) : number => x - y;
+
+console.log(sub(5,2))
+
+// Rest Parameters: type must be an array 
+
+function add2 (a: number, b: number, ...c:number[]) : number{
+    return a + b + c.reduce((x,y) => x + y, 0);
+}
+
+console.log(add2(2,3, ...[1,2,3,4,5]));
+console.log(add2(5,7,4,5,6,7))
+
+// Generic Functions
+// <Type> does not represent/mean anything, it is just a placeholder
+function getItems<Type>(items: Type[]): Type[]{
+    return new Array<Type>().concat(items);
+}
